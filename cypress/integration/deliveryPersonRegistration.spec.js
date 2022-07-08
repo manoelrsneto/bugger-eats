a521d6fed06b0e279497e241040f68e37bd793b5
Manoel Neto <manoelneto.dev@gmail.com>
2022-07-08 09:35:14 -0300

    Removing pages folder

--- cypress/integration/deliveryPersonRegistration.spec.js
+++ cypress/integration/deliveryPersonRegistration.spec.js
@@ -2,35 +2,35 @@
 
 describe("Suíte da página de Delivery", () => {
     beforeEach("Setup dos Testes", () => {
-        cy.log("Visitando página de cadastro de pessoa entregadora..")
-        cy.goTo("deliver")
-    })
+        cy.log("Visitando página de cadastro de pessoa entregadora..");
+        cy.goTo("deliver");
+    });
     it("Deve realizar cadastro com sucesso.", () => {
-        cy.log("Preenchendo informações pessoais.. 👨🏽")
-        cy.fillName("Zézinho")
-        cy.fillCPF("73268586082")
-        cy.fillEmail("zezinho@teste.com")
-        cy.fillWhatsapp("71912345678")
+        cy.log("Preenchendo informações pessoais.. 👨🏽");
+        cy.fillName("Zézinho");
+        cy.fillCPF("73268586082");
+        cy.fillEmail("zezinho@teste.com");
+        cy.fillWhatsapp("71912345678");
 
-        cy.log("Preenchendo informações de endereço.. 📝")
-        cy.fillCEP("41720400")
-        cy.clickSearchCEPButton()
-        cy.fillAdressNumber("572")
-        cy.fillAddressDetails("Cd. Greenville Apt. 204B")
+        cy.log("Preenchendo informações de endereço.. 📝");
+        cy.fillCEP("41720400");
+        cy.clickSearchCEPButton();
+        cy.fillAdressNumber("572");
+        cy.fillAddressDetails("Cd. Prédio Azul Apt. 205");
 
-        cy.log("Selecionando método de entrega.. 🏍 🚴🏾‍♀️ 🚐")
-        cy.selectDeliveryMethod("moto")
+        cy.log("Selecionando método de entrega.. 🏍 🚴🏾‍♀️ 🚐");
+        cy.selectDeliveryMethod("moto");
 
-        cy.log("Fazendo upload da CNH.. 🗃")
-        cy.uploadCNH()
+        cy.log("Fazendo upload da CNH.. 🗃");
+        cy.uploadCNH();
 
-        cy.log("Submetendo form.. ⬆️")
-        cy.clickSubmitButton()
-        
-        cy.log("Validando envio do form.. ✅")
-        cy.validateSuccessMessage()
+        cy.log("Submetendo form.. ⬆️");
+        cy.clickSubmitButton();
 
-        cy.log("Validando redirecionamento para a página principal.. 🏠")
-        cy.isOnHomePage()
-    })
-})
+        cy.log("Validando envio do form.. ✅");
+        cy.validateSuccessMessage();
+
+        cy.log("Validando redirecionamento para a página principal.. 🏠");
+        cy.isOnHomePage();
+    });
+});
